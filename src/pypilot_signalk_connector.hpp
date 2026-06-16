90aa57698fdb0eb91e7daa868cae62b4909bc688
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 00:54:53 -0400

    Add public SignalK connector include

---
+++ src/pypilot_signalk_connector.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "pypilot_signalk_connector/constants.hpp"
+#include "pypilot_signalk_connector/paths.hpp"
+#include "pypilot_signalk_connector/apply_path_value.hpp"
+#include "pypilot_signalk_connector/format_delta.hpp"
+#include "pypilot_signalk_connector/subscription.hpp"
+#include "pypilot_signalk_connector/put_control.hpp"
