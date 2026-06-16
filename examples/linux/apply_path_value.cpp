c87eeb7d8dbcc03e91e466b12c619b89d2e0efee
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 00:56:04 -0400

    Add Linux apply example

---
+++ examples/linux/apply_path_value.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <pypilot_signalk_connector.hpp>
+#include <pypilot_data_model.hpp>
+
+int main() {
+    pypilot_data_model::DataModel<> model;
+    pypilot_signalk_connector::SignalKConnector<> connector;
+    connector.apply_number(pypilot_signalk_connector::SignalKPath::wind_speed_apparent,
+                           5.144456333854638f,
+                           model,
+                           1000000);
+    std::cout << "apparent wind speed kn=" << model.wind.apparent.speed_kn.value << "\n";
+    return 0;
+}
