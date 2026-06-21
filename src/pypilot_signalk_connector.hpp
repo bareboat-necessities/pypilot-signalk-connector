7b0845adde3d91f51258a7e5de92ab760d86b6ae
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-21 15:02:21 -0400

    Export expanded Signal K connector helpers

--- src/pypilot_signalk_connector.hpp
+++ src/pypilot_signalk_connector.hpp
@@ -5,7 +5,13 @@
 #include "pypilot_signalk_connector/apply_path_value.hpp"
 #include "pypilot_signalk_connector/format_delta.hpp"
 #include "pypilot_signalk_connector/subscription.hpp"
+#include "pypilot_signalk_connector/unsubscribe.hpp"
 #include "pypilot_signalk_connector/put_control.hpp"
+#include "pypilot_signalk_connector/put_messages.hpp"
+#include "pypilot_signalk_connector/json_delta.hpp"
+#include "pypilot_signalk_connector/outbound_delta.hpp"
+#include "pypilot_signalk_connector/transport.hpp"
+#include "pypilot_signalk_connector/discovery_endpoint.hpp"
 
 #if defined(PYPILOT_SIGNALK_CONNECTOR_WITH_MDNS)
 #include "pypilot_signalk_connector/mdns_discovery.hpp"
