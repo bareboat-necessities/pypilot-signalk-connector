d4ae4798ce1f07633bbadd405984467823ff19da
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-21 14:22:13 -0400

    Only export mdns helper when dependency is enabled

--- src/pypilot_signalk_connector.hpp
+++ src/pypilot_signalk_connector.hpp
@@ -6,4 +6,7 @@
 #include "pypilot_signalk_connector/format_delta.hpp"
 #include "pypilot_signalk_connector/subscription.hpp"
 #include "pypilot_signalk_connector/put_control.hpp"
+
+#if defined(PYPILOT_SIGNALK_CONNECTOR_WITH_MDNS)
 #include "pypilot_signalk_connector/mdns_discovery.hpp"
+#endif
